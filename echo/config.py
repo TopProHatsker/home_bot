752fb3686a28d53e800a621d8c28fb16e243be17
Artem Karchenko <artem.karchenko@gmail.com>
2019-08-17 12:00:41 +0300

    Start bug fixed

--- echo/config.py
+++ echo/config.py
@@ -1,2 +1,4 @@
-TG_TOKEN = "956389731:AAFMpxnFFd0RqqC25fgxiMRQrOQMPZKRAxQ"
+
+p = input("Passwd: ")
+TG_TOKEN = "956389731:AAFMpxnFFd0RqqC25fgxiMRQrOQMPZK"+p
 TG_API_URL = "https://telegg.ru/orig/bot"
